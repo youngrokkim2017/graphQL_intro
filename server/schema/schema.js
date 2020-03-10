0cd69366f6a5ca136edaf01540460c9ee27805af
Patrick <patrick.yrkim@gmail.com>
2020-03-10 16:23:04 -0700

    Add type relations between books and authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const _ = require('lodash');
 
 // test data
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
 ];
 
 var authors = [
@@ -30,6 +30,13 @@ const BookType = new GraphQLObjectType({ // defined object type, book
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                // console.log(parent);
+                return _.find(authors, { id: parent.authorId });
+            }
+        },
     })
 });
 
