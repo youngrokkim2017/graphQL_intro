a6ac00545fae9a027ab294dc0da0be9544bc18fb
Patrick <patrick.yrkim@gmail.com>
2020-03-10 15:47:49 -0700

    Update resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,16 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+const _ = require('lodash');
+
+// test data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
+//
+
 const BookType = new GraphQLObjectType({ // defined object type, book
     name: 'Book',
     fields: () => ({
@@ -18,6 +28,10 @@ const RootQuery = new GraphQLObjectType({  // this is how you jump into the grap
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {  // tells graphql how to get the data when you make a request
                 // code to get data from db / other source
+
+                // use the id arg and find the book
+
+                return _.find(books, { id: args.id });
             }
         }
     })
