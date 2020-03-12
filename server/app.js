f74a332f33adb647617d92594a05c30112251206
Patrick <patrick.yrkim@gmail.com>
2020-03-11 17:14:04 -0700

    Add mongoDB connection and initialize mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connecting to mongoDB
+mongoose.connect('add mongoDB string here');
+mongoose.connection.once('open', () => {  // event listener
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({  // middleware
     schema,  // can use the schema to map out the map
