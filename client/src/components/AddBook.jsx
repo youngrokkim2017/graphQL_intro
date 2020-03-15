141f5504f496bea6e5fb7d481a47523b532cf563
Patrick <patrick.yrkim@gmail.com>
2020-03-14 19:05:41 -0700

    Add query variables

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -51,7 +51,13 @@ class AddBook extends React.Component {
 
         // console.log(this.state);
 
-        this.props.addBookMutation();
+        this.props.addBookMutation({
+            variables: {
+                name: this.state.name,
+                genre: this.state.genre,
+                authorId: this.state.authorId,
+            }
+        });
     }
 
     render() {
