31fd85ca179ab69071b72507f5272de68e6ad698
Patrick <patrick.yrkim@gmail.com>
2020-03-14 18:06:14 -0700

    Add render data in book component

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -18,14 +18,31 @@ class BookList extends React.Component {
     //     super(props);
     // }
 
+    displayBooks() {
+        var data = this.props.data;
+
+        if (data.loading) {
+            return (
+                <div>Loading books...</div>
+            );
+        } else {
+            return data.books.map(book => {
+                return (
+                    <li key={book.id}>
+                        {book.name}
+                    </li>
+                )
+            });
+        }
+    }
+
     render() {
-        console.log(this.props);
+        // console.log(this.props);
+
         return (
             <div>
                 <ul id="book-list">
-                    <li>
-                        Book name
-                    </li>
+                    {this.displayBooks()}
                 </ul>
             </div>
         )
